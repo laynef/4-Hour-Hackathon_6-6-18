47b2292e114fdae4bb6ed7994ac2187071b24835
Layne Faler <laynefaler@gmail.com>
2018-06-06 15:09:22 -0700

    Updates to api

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const { each } = require('lodash')
+
+const express = require('express');
+const http = require('http')
+const cors = require('cors')
+const helmet = require('helmet')
+const apiVersions = require('./routes')
+
+
+const app = express();
+
+app.use(cors())
+app.use(helmet())
+
+each(apiVersions, (value, key) => {
+    app.use(`/${key}`, value)
+})
+
+const server = http.createServer(app)
+
+server.listen(8080)
