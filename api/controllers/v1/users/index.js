94cbe416764ad0dffc1d6ee5be116b9b6a869ebe
Layne Faler <laynefaler@gmail.com>
2018-06-06 14:11:49 -0700

    Working on api

--- api/controllers/v1/users/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const index = fs.readdirSync(__dirname).reduce((acc, item) => {
-    acc[item] = require(`${__dirname}/${item}`);
-    return acc;
-}, {});
-
-module.exports = index;
