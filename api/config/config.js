ba5552bd1a5759df7e63e3db70df08ace15c0582
Layne Faler <laynefaler@gmail.com>
2018-06-06 13:24:17 -0700

    Added user table in pg

---
+++ api/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "pg",
+    "port": 5430
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "pg",
+    "port": 5430
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "pg",
+    "port": 5430
+  }
+}
