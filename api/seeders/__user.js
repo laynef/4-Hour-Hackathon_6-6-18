f52165bb22a0c578d2a31786cc1c776948f320d8
Layne Faler <laynefaler@gmail.com>
2018-06-06 15:39:14 -0700

    Created three users

--- api/seeders/__user.js
+++ api/seeders/__user.js
@@ -1,8 +1,31 @@
-'use strict';
+const { genSaltSync, hashSync } = require('bcrypt-nodejs')
+const { image } = require('faker')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('Users', []);
+      const password = 'pass1234'
+      const users = [
+          {
+              email: 'jordan@email.com',
+              password: hashSync(password, genSaltSync(10)),
+              image: image.imageUrl(500, 500),
+          },
+          {
+            email: 'chris@email.com',
+            password: hashSync(password, genSaltSync(10)),
+            image: image.imageUrl(500, 500),
+        },
+        {
+            email: 'alex@email.com',
+            password: hashSync(password, genSaltSync(10)),
+            image: image.imageUrl(500, 500),
+        },
+      ]
+      return queryInterface.bulkInsert('Users', users.map((e) => ({
+          ...e,
+          created_at: new Date(),
+          updated_at: new Date(),
+      })));
   },
 
   down: (queryInterface, Sequelize) => {
